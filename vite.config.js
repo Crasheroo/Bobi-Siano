b57de2ef50888a75ef293737f10b8a98ccc9ff9d
Paweł Zalewski <119087911+Crasheroo@users.noreply.github.com>
2026-05-13 00:15:10 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Zmień 'lucent' na nazwę swojego repozytorium na GitHubie
-const REPO_NAME = 'lucent'
+const REPO_NAME = 'Bobi-Siano'
 
 export default defineConfig({
   plugins: [react()],
